bbc8c4b4f5e6463d7554b1043c6711d1e7e4f5cb
gpsign <gpsignorini@gmail.com>
2023-04-13 16:35:09 -0300

    Login e leitura de chat

--- JS/script.js
+++ JS/script.js
@@ -1,33 +1,64 @@
+axios.defaults.headers.common["Authorization"] = "6SlanhOQ22CQz43wqstq4NsK";
 const chat = document.querySelector(".chat");
+var user = {
+  name: "",
+};
 
-function notification(user, login) {
-  let inf = "";
-  if (login) inf = "entra na";
-  else inf = "sai da";
-  return `<div class="message notification">
-            <p>
-                <m-time>(10:52:52)</m-time> <m-bold>${user} </m-bold> ${inf} sala...
-            </p>
-          </div>`;
+login();
+
+function login() {
+  user.name = prompt("Qual o seu nome?");
+  let promLogin = axios.post(
+    "https://mock-api.driven.com.br/api/vm/uol/participants",
+    user
+  );
+
+  promLogin.then(online);
+  promLogin.catch(login);
 }
 
-function message(user, priv, message) {
-  let text = "";
-  if (priv !== "Todos") {
-    text = `<div class="message private">
+function online(ans) {
+  setInterval(
+    () => axios.post("https://mock-api.driven.com.br/api/vm/uol/status", user),
+    5000
+  );
+  setInterval(getData, 3000);
+}
+
+function getData() {
+  let promData = axios.get(
+    "https://mock-api.driven.com.br/api/vm/uol/messages"
+  );
+  promData.then(render);
+  promData.catch(() => alert("Erro"));
+}
+
+function render(chatContent) {
+  chat.innerHTML = "";
+  for (let i = 0; i < chatContent.data.length; i++) {
+    sendChat(chatContent.data[i]);
+  }
+  if (chat.scrollTop > 7200) chat.scrollTop = chat.scrollHeight;
+}
+
+function sendChat(info) {
+  let skel = "";
+
+  if (info.type == "status") {
+    skel = `<div class="message notification">
     <p>
-    <m-time>(09:22:48)</m-time>
-    <m-bold>${user}</m-bold>reservadamente para<m-bold>${priv}: </m-bold>`;
-  } else
-    text = `<div class="message">
-                <p>
-                    <m-time>(10:52:52)</m-time> <m-bold>${user}</m-bold> para
-                    <m-bold>Todos: </m-bold>`;
-  text += message + "</p></div>";
-  console.log(text);
-  return text;
-}
-
-function sendChat(content) {
-  chat.innerHTML += content;
+        <m-time>(${info.time})</m-time> <m-bold>${info.from} </m-bold>`;
+  } else if (info.to !== "Todos") {
+    skel = `<div class="message private">
+    <p>
+    <m-time>(${info.time})</m-time>
+    <m-bold>${info.from}</m-bold>reservadamente para<m-bold>${info.to}: </m-bold>`;
+  } else {
+    skel = `<div class="message">
+              <p>
+                  <m-time>(${info.time})</m-time> <m-bold>${info.from}</m-bold> para
+                  <m-bold>Todos: </m-bold>`;
+  }
+  skel += info.text + "</p></div>";
+  chat.innerHTML += skel;
 }
