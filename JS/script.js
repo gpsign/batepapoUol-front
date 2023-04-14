9b691099082cc728ba43366d46bcebf5edaaa1dd
gpsign <gpsignorini@gmail.com>
2023-04-14 11:43:29 -0300

    Changelog: + Bugfix de adicionar quebra de linha ao enviar com enter. + Corrigido render de quebra de linha na mensagem.

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,24 @@
 axios.defaults.headers.common["Authorization"] = "6SlanhOQ22CQz43wqstq4NsK";
 const chat = document.querySelector(".chat");
 const txt = document.querySelector("textarea");
-var holdingCtrl = false;
 var user = {
   name: "",
 };
 var first = true;
 
-txt.addEventListener('keydown', function(pressed){
-  if(pressed.key === 'Control')holdingCtrl = true;
-  else if(pressed.key === 'Enter' && !holdingCtrl)sendMessage();
-  else if(pressed.key === 'Enter' && holdingCtrl)txt.value += '\n';
-});
+txt.addEventListener("keydown", function (pressed) {
+  if (pressed.key == "Enter" && !pressed.ctrlKey) {
+    if (!pressed.repeat) {
+      sendMessage();
+    }
 
-txt.addEventListener('keyup', function(released){
-  if(released.key == 'Control')holdingCtrl = false;
+    pressed.preventDefault(); // Prevents the addition of a new line in the text field
+  }
+  if (pressed.key == "Enter" && pressed.ctrlKey) {
+    if (!pressed.repeat) {
+      txt.value += "\n";
+    }
+  }
 });
 
 login();
@@ -50,43 +54,58 @@ function getData() {
 function render(chatContent) {
   chat.innerHTML = "";
   chatContent.data.forEach(applyToChat);
-  if(first){
+  if (first) {
     chat.scrollTop = chat.scrollHeight;
     first = false;
-  }
-  else if (chat.scrollTop > 7200) chat.scrollTop = chat.scrollHeight;
+  } else if (chat.scrollTop > 7200) chat.scrollTop = chat.scrollHeight;
 }
 
 function applyToChat(info) {
   let bp = "";
 
   if (info.type == "status") {
-
-    bp = `<div class="message notification" data-test="message">
+    bp = `<div class="notification message" data-test="message">
+        <p>
         <m-time>(${info.time})</m-time> <m-bold>${info.from} </m-bold>`;
   } else if (info.type == "private_message") {
-    bp = `<div class="message private" data-test="message">
+    bp = `<div class="private message" data-test="message">
+    <p>
     <m-time>(${info.time})</m-time>
     <m-bold>${info.from}</m-bold>reservadamente para<m-bold>${info.to}: </m-bold>`;
-  } else{
+  } else {
     bp = `<div class="message" data-test="message">
+    <p>
                   <m-time>(${info.time})</m-time> <m-bold>${info.from}</m-bold> para
                   <m-bold>Todos: </m-bold>`;
   }
-  bp += info.text + "</div>";
+  bp += info.text.replaceAll('\n', '<br>') + "</p></div>";
   chat.innerHTML += bp;
 }
 
-function sendMessage(){
-    let msg = {
-        from: user.name,
-        to: "Todos",
-        text: txt.value,
-        type: "message"
-    }
-    txt.value = '';
+function sendMessage() {
+  if (!validMessage()) {
+    txt.value = "";
+    return;
+  }
+  let msg = {
+    from: user.name,
+    to: "Todos",
+    text: txt.value,
+    type: "message",
+  };
+  txt.value = "";
 
-    let promSend = axios.post("https://mock-api.driven.com.br/api/vm/uol/messages", msg);
-    promSend.then(getData);
-    promSend.catch(() => window.location.reload(true));
+  let promSend = axios.post(
+    "https://mock-api.driven.com.br/api/vm/uol/messages",
+    msg
+  );
+  promSend.then(getData);
+  promSend.catch(() => window.location.reload(true));
+}
+
+function validMessage() {
+  for (let i = 0; i < txt.value.length; i++)
+    if (txt.value[i] !== "\n" && txt.value[i] !== "") return true;
+
+  return false;
 }
